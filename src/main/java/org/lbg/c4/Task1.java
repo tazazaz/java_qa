d267dd87bcdc5dfe899995f00162d23d9f8ac711
Tarran <t.s.moore1@outlook.com>
2024-10-29 09:27:37 +0000

    qa basic java work

---
+++ src/main/java/org/lbg/c4/Task1.java
@@ -0,0 +1,43 @@
+package org.lbg.c4;
+import java.util.Scanner;
+
+public class Task1 {
+
+    public static void main(String[] ars) {
+        /*
+
+        String firstName = "Tarran";
+        String lastName = "Moore";
+
+        String name = firstName + " " + lastName;
+
+        System.out.println(name);
+
+
+
+        int num1 = 1;
+        int num2 = 2;
+
+        System.out.println(num1 + num2);
+
+        double result = (double) num1 / num2;
+
+        System.out.println(result);
+
+        int num3 = 8;
+
+        System.out.println("num3++:" + " " + num3++);
+        System.out.println("num3:" + " " + num3);
+        System.out.println("++num3:" + " " + num3);
+
+        */
+
+
+        Scanner scanner = new Scanner(System.in);
+
+
+
+
+    }
+
+}
